4ac08a9a256a8be99482e4dc5ad10df0abeb806e
Gábor László <laszlogabor91@gmail.com>
2017-08-06 15:48:27 +0200

    Update

--- Microscale-Lego-buildings/lego_linedupl.py
+++ Microscale-Lego-buildings/lego_linedupl.py
@@ -1,26 +1,25 @@
+"""      
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 21 15:00:55 2016
-
 @author: ASUS
+Created on Sun Aug 21 15:00:55 2016
 """
 
-filename = "borondos.ldr"
-
-text = open(filename, "r")
-lego = text.read()
-text.close()
-
-legoList = lego.split("\n")
-
-print len(legoList)
-print len(list(set(legoList)))
-
-final = ""
-
-for i in legoList:
-    final += i + "\n"
-
-text = open(filename, "w")
-text.write(final)
-text.close()
+import os 
+for filename in os.listdir(os.curdir):
+    if ".ldr" in filename:
+        text = open(filename, "r")
+        lego = text.read()
+        text.close()
+        legoList = lego.split("\n")
+        if len(legoList) != len(list(set(legoList))):
+            final = ""
+            legoList = sorted(list(set(legoList))) 
+            for line in legoList:
+                if line != "":
+                   final += line + "\n"    
+       
+            print len(legoList), len(list(set(legoList)))
+            text = open(filename, "w")
+            text.write(final)
+            text.close()
+        print filename, len(legoList)
